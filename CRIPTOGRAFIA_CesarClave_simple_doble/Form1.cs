480f650256bfc948d32d60fd23ddad8b0c726bbe
MAYL H.C <herbascaseresguido@gmail.com>
2023-10-05 17:01:54 -0400

    Agregar archivos de proyecto.

---
+++ CRIPTOGRAFIA_CesarClave_simple_doble/Form1.cs
@@ -0,0 +1,10 @@
+namespace CRIPTOGRAFIA_CesarClave_simple_doble
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
