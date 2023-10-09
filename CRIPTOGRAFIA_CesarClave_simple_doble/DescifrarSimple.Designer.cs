65be08ae67a45236809f07f50ec26661ac1d5bcd
MAYL H.C <herbascaseresguido@gmail.com>
2023-10-09 17:18:06 -0400

    Se creo y modifico las interfaces para el desarrollo de cifrado y descifado de Transposicion Simple

---
+++ CRIPTOGRAFIA_CesarClave_simple_doble/DescifrarSimple.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace CRIPTOGRAFIA_CesarClave_simple_doble
+{
+    partial class DescifrarSimple
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            button3 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Symbol", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(12, 22);
+            label1.Name = "label1";
+            label1.Size = new Size(176, 21);
+            label1.TabIndex = 2;
+            label1.Text = "DESCIFRAR CIFRADO";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(44, 70);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(388, 23);
+            textBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Symbol", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(72, 116);
+            label2.Name = "label2";
+            label2.Size = new Size(240, 21);
+            label2.TabIndex = 4;
+            label2.Text = "RESULTADO DE DESCIFRADO";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(111, 164);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(321, 23);
+            textBox2.TabIndex = 5;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Segoe UI Historic", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button3.Location = new Point(187, 264);
+            button3.Name = "button3";
+            button3.Size = new Size(125, 33);
+            button3.TabIndex = 7;
+            button3.Text = "VOLVER";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // DescifrarSimple
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(529, 338);
+            Controls.Add(button3);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "DescifrarSimple";
+            Text = "DescifrarSimple";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private Label label2;
+        private TextBox textBox2;
+        private Button button3;
+    }
+}
---
+++ CRIPTOGRAFIA_CesarClave_simple_doble/TransSimple.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace CRIPTOGRAFIA_CesarClave_simple_doble
+{
+    partial class TransSimple
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            button2 = new Button();
+            button3 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI Historic", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(158, 216);
+            button1.Name = "button1";
+            button1.Size = new Size(166, 41);
+            button1.TabIndex = 0;
+            button1.Text = "CIFRAR";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Symbol", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(12, 21);
+            label1.Name = "label1";
+            label1.Size = new Size(194, 21);
+            label1.TabIndex = 1;
+            label1.Text = "PALABRA PARA CIFRAR";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 56);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(388, 23);
+            textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Symbol", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(48, 99);
+            label2.Name = "label2";
+            label2.Size = new Size(209, 21);
+            label2.TabIndex = 3;
+            label2.Text = "RESULTADO DE CIFRADO";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(70, 158);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(388, 23);
+            textBox2.TabIndex = 4;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI Historic", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.Location = new Point(392, 293);
+            button2.Name = "button2";
+            button2.Size = new Size(125, 33);
+            button2.TabIndex = 5;
+            button2.Text = "DESCIFRAR";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Segoe UI Historic", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button3.Location = new Point(261, 293);
+            button3.Name = "button3";
+            button3.Size = new Size(125, 33);
+            button3.TabIndex = 6;
+            button3.Text = "VOLVER";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // TransSimple
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(529, 338);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Name = "TransSimple";
+            Text = "TransSimple";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label label1;
+        private TextBox textBox1;
+        private Label label2;
+        private TextBox textBox2;
+        private Button button2;
+        private Button button3;
+    }
+}
